c79f715d95ef38f2f2535611a87f2bf7b4be7f77
一心令狐 <3485777351@qq.com>
2018-10-21 02:32:58 +0800

    O(n)

--- 1. Two Sum/main.py
+++ 1. Two Sum/main.py
@@ -1,15 +1,16 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for index1,i in enumerate(nums):
-            for index2,j in enumerate(nums[index1+1:]):
-                if i + j == target:
-                    return [index1,index1+1+index2]
-if __name__ == '__main__':
-    d = Solution()
-    result = d.twoSum([3, 2, 4,],6)
-    print(result)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); ++i) {
+            auto got = m.find(target - nums[i]);
+            if (got != m.end()) {
+                vector<int> result {got->second, i};
+                return result;
+            } else {
+                m[nums[i]] = i;
+            }
+        }
+        return vector<int>{-1};
+    }
+};
