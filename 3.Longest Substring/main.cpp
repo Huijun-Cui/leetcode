309c89e6f5adeb466dc6894cd8c490b8452ff331
cui <3485777351@qq.com>
2018-10-21 22:28:20 +0800

    initial

---
+++ 3.Longest Substring/main.cpp
@@ -0,0 +1,57 @@
+
+
+# beat 30%
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        map<char,int> dict; // <number,index>
+        int index = 0;
+        int max = 0;
+        int current_length = 0;
+        int pre_index = 0;
+        for(auto c:s)
+        {
+
+            if(dict.find(c) == dict.end())
+            {
+                dict[c] = index;
+                current_length +=1;
+                if(current_length > max)
+                    max = current_length;
+            }
+
+            else
+            {
+                if((index - dict[c]) > max)
+                    max = index - dict[c];
+
+
+                int target_char_index = dict[c];
+
+                for(int del_index = 0 + pre_index;del_index <= target_char_index;del_index++)
+                {
+
+                    dict.erase(s[del_index]);
+                    current_length -=1;
+
+
+                }
+
+
+                pre_index = target_char_index + 1;
+                dict[c] = index;
+                current_length +=1;
+                if(current_length > max)
+                    max = current_length;
+
+
+
+            }
+            index +=1;
+        }
+        return max;
+
+    }
+};
+
