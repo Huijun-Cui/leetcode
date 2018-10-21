d2869a2d1e4cddbe7c05b2631feee6611bb101f7
一心令狐 <3485777351@qq.com>
2018-10-21 19:29:49 +0800

    Update main.py

--- 1. Two Sum/main.py
+++ 1. Two Sum/main.py
@@ -1,16 +1,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> m;
-        for (int i = 0; i < nums.size(); ++i) {
-            auto got = m.find(target - nums[i]);
-            if (got != m.end()) {
-                vector<int> result {got->second, i};
-                return result;
-            } else {
-                m[nums[i]] = i;
+        // Only really have to search if there's more than 2 elements...
+        if(nums.size() > 2){
+            // So... the known solution is to use a hashmap with the stored differences...
+            map<int,int> summer; // <number,index>
+            for(int i=0;i<nums.size();i++){
+                int theDiff = target-nums[i];
+                if(summer.find(theDiff)==summer.end()){
+                    summer[nums[i]]=i;
+                } else {
+                    return {summer[theDiff],i};
+                }
             }
         }
-        return vector<int>{-1};
+        //If there's only 2 elements, obviously that's the answer then...
+        return {0,1};
     }
 };
+
