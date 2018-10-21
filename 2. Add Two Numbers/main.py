2371ceb8a1ce87596889223eade98fe965a6165f
一心令狐 <3485777351@qq.com>
2018-10-21 19:24:58 +0800

    Update main.py

--- 2. Add Two Numbers/main.py
+++ 2. Add Two Numbers/main.py
@@ -1,19 +1,36 @@
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+#include <iostream>
+#include <map>
+#include <vector>
+#include <typeinfo>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        cf = 0
-        while l1.next != None and l2.next != None:
-            cf = int((l1.val+l2.val + cf)/10)
-            returnNode = (l1.val + l2.val + cf) % 10
-            l1 = l1.next
-            l2 = l2.next
-        if l1.next !=None
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode preHead(0),*p = &preHead;
+        int cf = 0;
+        while(l1 || l2 || cf)
+        {
+            if(l1 == nullptr) l1 = new ListNode(0);
+            if(l2 == nullptr) l2 = new ListNode(0);
+            int sum = l1->val + l2->val;
+            cf = sum / 10;
+            p->next = new ListNode(sum % 10);
+            p = p->next;
+            l1 = l1->next;
+            l2 = l2->next;
+
+        }
+        return preHead.next;
+
+    };
+};
