21f039243daaa08d19759ee298d51bd7f447c270
cui <3485777351@qq.com>
2018-10-22 22:06:38 +0800

    initial

---
+++ 8.String to Integer/main.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int myAtoi(string str) {
+        int index = 0;
+        while(index<str.size() && str[index] == ' ') index +=1;
+        if(index == str.size() || (!isdigit(str[index]) &&str[index] !='-'&&str[index]!='+')) return 0;
+        if
+
+
+        long num = 0;
+        int flag = 1;
+        if(str[index] == '-')
+        {
+            flag = -1;
+            index +=1;
+        }
+        if(str[index] == '+')
+        {
+            flag = 1;
+            index +=1;
+        }
+        while(index<str.size() && isdigit(str[index]))
+        {
+            num *= 10;
+            num += str[index] - '0';
+            index +=1;
+        }
+        if(num * flag > INT_MAX) return INT_MAX;
+        if(num * flag < INT_MIN) return INT_MIN;
+        return num * flag;
+
+}
+};
