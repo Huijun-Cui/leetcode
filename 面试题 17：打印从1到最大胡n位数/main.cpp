99a1aa47ff6309693c13277401de6af670dfb336
cui <3485777351@qq.com>
2018-10-30 17:56:57 +0800

    initial

--- 面试题 17：打印从1到最大胡n位数/main.cpp
+++ 面试题 17：打印从1到最大胡n位数/main.cpp
@@ -44,3 +44,19 @@ bool Increment(char* number)
     }
     return isOverflow;
 }
+
+void PrintNumber(char* number)
+{
+    bool isBeginning0 = true;
+    int nLength = strlen(number);
+    for(int i =0;i<nLength;++i)
+    {
+        if(isBeginning0 && number[i]!=0)
+            isBeginning0 = false;
+        if(!isBeginning0)
+        {
+            printf("%c",number[i]);
+        }
+    }
+    printf("\t");
+}
