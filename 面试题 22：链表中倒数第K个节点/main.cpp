ca5ee8bdef3de5effc737d616bc1e40ebdf38044
cui <3485777351@qq.com>
2018-10-31 15:35:43 +0800

    initial

--- 面试题 22：链表中倒数第K个节点/main.cpp
+++ 面试题 22：链表中倒数第K个节点/main.cpp
@@ -5,11 +5,16 @@ struct ListNode
 };
 ListNode* FindKthToTail(ListNode* pListHead, unsigned int k)
 {
+    if(pListHead == nullptr || k== 0)
+        return nullptr;
     ListNode* pAhead = pListHead;
     ListNode* pBehind = nullptr;
     for(unsigned int i=0;i<k-1;i++)
     {
-        pAhead = pAhead->m_pNext;
+        if(pAhead->m_pNext != nullptr)
+            pAhead = pAhead->m_pNext;
+        else
+            return nullptr;
     }
     pBehind = pListHead;
     while(pAhead->m_pNext != nullptr)
