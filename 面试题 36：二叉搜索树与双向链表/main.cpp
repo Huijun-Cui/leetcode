1bc54ca5d77e123fd9a373ac27c901a1c806e69d
cui <3485777351@qq.com>
2018-11-01 21:38:54 +0800

    initial

---
+++ 面试题 36：二叉搜索树与双向链表/main.cpp
@@ -0,0 +1,35 @@
+struct BinaryTreeNode
+{
+    int m_nValue;
+    BinaryTreeNode* m_pLeft;
+    BinaryTreeNode* m_pRight;
+};
+
+BinaryTreeNode* Conver(BinaryTreeNode* pRootOfTree)
+{
+    BinaryTreeNode* pLastNodeInList = nullptr;
+    ConvertNode(pRootOfTree,&pLastNodeInList);
+
+    BinaryTreeNode* pHeadOfList = pLastNodeInList;
+    while(pHeadOfList != nullptr && pHeadOfList->m_pLeft != nullptr)
+        pHeadOfList = pHeadOfList->m_pLeft;
+    return pHeadOfList;
+
+}
+
+
+void ConvertNode(BinaryTreeNode* pNode,BinaryTreeNode** pLastNodeInList)
+{
+    if(pNode == nullptr)
+        return;
+    BinaryTreeNode* pCurrent = pNode;
+    if(pCurrent->m_pLeft != nullptr)
+        ConvertNode(pCurrent->m_pLeft,pLastNodeInList);
+    pCurrent->m_pLeft = *pLastNodeInList;
+    if(*pLastNodeInList != nullptr)
+        (*pLastNodeInList)->m_pRight = pCurrent;
+    *pLastNodeInList = pCurrent;
+
+    if(pCurrent->m_pRight != nullptr)
+        ConvertNode(pCurrent->m_pRight,pLastNodeInList);
+}
